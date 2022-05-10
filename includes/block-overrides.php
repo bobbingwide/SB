9092ef2cb5a3cc9d79a763ef8d32e1a8ebd70aa9
Herb Miller <herb@bobbingwide.com>
2022-05-10 22:16:14 +0100

    Override navigation-link block #30

--- includes/block-overrides.php
+++ includes/block-overrides.php
@@ -22,7 +22,7 @@ require_once __DIR__ . '/post-author.php';
 //require_once __DIR__ . '/post-content.php';
 require_once __DIR__ . '/template-part.php';
 //require_once __DIR__ . '/navigation.php';
-//require_once __DIR__ . '/navigation-link.php';
+require_once __DIR__ . '/navigation-link.php';
 //require_once __DIR__ . '/post-hierarchical-terms.php';
 //require_once __DIR__ . '/block.php';
 require_once __DIR__ . '/tag-cloud.php';
@@ -39,18 +39,18 @@ add_filter( 'register_block_type_args', 'sb_register_block_type_args', 9 );
  * @return array
  */
 function sb_register_block_type_args( $args ) {
-    $args = sb_maybe_override_block(  $args,'core/query-pagination', 'render_block_core_query_pagination');
-    $args = sb_maybe_override_block(  $args,'core/query-pagination-numbers', 'render_block_core_query_pagination_numbers');
+    //$args = sb_maybe_override_block(  $args,'core/query-pagination', 'render_block_core_query_pagination');
+    //$args = sb_maybe_override_block(  $args,'core/query-pagination-numbers', 'render_block_core_query_pagination_numbers');
 
     $args = sb_maybe_override_block(  $args,'core/post-template', 'render_block_core_post_template' );
 	$args = sb_maybe_override_block(  $args,'core/post-author', 'render_block_core_post_author' );
-    $args = sb_maybe_override_block(  $args,'core/post-excerpt', 'render_block_core_post_excerpt' );
-    $args = sb_maybe_override_block(  $args,'core/post-content', 'render_block_core_post_content' );
+    //$args = sb_maybe_override_block(  $args,'core/post-excerpt', 'render_block_core_post_excerpt' );
+    //$args = sb_maybe_override_block(  $args,'core/post-content', 'render_block_core_post_content' );
     $args = sb_maybe_override_block(  $args,'core/template-part', 'render_block_core_template_part' );
-    $args = sb_maybe_override_block(  $args,'core/navigation', 'render_block_core_navigation' );
+    //$args = sb_maybe_override_block(  $args,'core/navigation', 'render_block_core_navigation' );
     $args = sb_maybe_override_block(  $args,'core/navigation-link', 'render_block_core_navigation_link' );
-    $args = sb_maybe_override_block(  $args,'core/post-hierarchical-terms', 'render_block_core_post_hierarchical_terms' );
-    $args = sb_maybe_override_block(  $args,'core/block', 'render_block_core_block' );
+    //$args = sb_maybe_override_block(  $args,'core/post-hierarchical-terms', 'render_block_core_post_hierarchical_terms' );
+    //$args = sb_maybe_override_block(  $args,'core/block', 'render_block_core_block' );
     $args = sb_maybe_override_block(  $args,'core/tag-cloud', 'render_block_core_tag_cloud' );
     return $args;
 }
